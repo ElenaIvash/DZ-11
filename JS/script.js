4bb09c3faba75ce853f315956d0d0eeefc623e5d
Elena <77905432@mail.ru>
2022-04-17 16:07:28 +0300

    login form implementation
    Я закоммитила задания, чтобы поочередно проверить каждое

---
+++ JS/script.js
@@ -0,0 +1,88 @@
+/*let a = +prompt('Введите число', "");
+if (a == 0) {
+    alert("Верно");
+} else {
+    alert("Неверно");
+}*/
+
+/*let b = +prompt('Введите число', "");
+let c = b > 0 ? "Верно" : "Неверно";
+alert(c);*/
+
+/*let a = +prompt('Введите число', "");
+let c = a < 0 ? "Верно" : "Неверно";
+alert(c);*/
+
+/*let a = +prompt('Введите число', "");
+let b = a >= 0 ? "Верно" : "Неверно";
+alert(b);*/
+
+/*let a = prompt("Введите число", "");
+if (a >= 0) {
+    alert("Верно");
+} else {
+    alert("Неверно");
+}*/
+
+/*let a = prompt("Введите число");
+let b = a != 0 ? "Верно" : "Неверно";
+alert(b);*/
+
+/*let a = prompt("Введите слово test");
+if (a == "test") {
+    alert("Верно");
+} else {
+    alert("Неверно");
+}*/
+
+/*let a = +prompt("Введите число");
+let b = a === 1 ? "Верно" : "Неверно";
+alert(b);*/
+
+/*let a = prompt("Введите число");
+let b = a > 0 && 5 ? "Верно" : "Неверно";
+alert(b);*/
+
+/*let a = +prompt("Введите число", "");
+switch(a) {
+    case 0:
+        alert(a + 7);
+        break;
+        case 2:
+            alert(a + 7);
+            break;
+            default:
+                alert(a / 10);  
+}*/
+
+/*let a = +prompt("Введите первое число");
+let b = +prompt("Введите второе число");
+if (a <= 1 && b >= 3) {
+    alert(a + b);
+} else {
+alert(a - b);
+}*/
+
+/*let a = +prompt("Введите первое число");
+let b = +prompt("Введите второе число");
+if (a > 2 && a < 11 || b >= 6 && b < 14) {
+    alert("Верно");
+} else {
+alert("Неверно");
+}*/
+
+/*let a = +prompt("Введите значение от 1 до 4");
+switch(a) {
+    case 1:
+        alert("Result: Зима");
+        break;
+        case 2:
+        alert("Result: Весна");
+        break;
+        case 3:
+        alert("Result: Лето");
+        break;
+        case 4:
+        alert("Result: Осень");
+        break;
+}*/
